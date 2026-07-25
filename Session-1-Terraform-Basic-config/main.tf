63d50a4b91842f977dc23561348c228ccd703472
MultiCloud-Tech-DevSecOps <younisgenai@gmail.com>
2026-07-25 14:40:07 +0100

    Refactor comments for clarity in main.tf

--- Session-1-Terraform-Basic-config/main.tf
+++ Session-1-Terraform-Basic-config/main.tf
@@ -1,5 +1,7 @@
 
-# the following code will create 3 EC2 instances in AWS with the specified AMI and instance type. Each instance will be tagged with a name and environment for easy identification.
+# the following code will create 3 EC2 instances in AWS with the specified AMI and instance type.
+# Each instance will be tagged with a name and environment for easy identification.
+
 resource "aws_instance" "web" {
   count         = 3 # Number of instances to create
   ami           = "ami-0150189e4c09ffab5" # Example Amazon Linux 2023 AMI (verify for your region)
