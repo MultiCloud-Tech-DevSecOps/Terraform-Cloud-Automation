d520acbd0fd7d318d45fb2b2f2ff7dc725995ac1
MultiCloud-Tech-DevSecOps <younisgenai@gmail.com>
2026-08-24 13:49:12 +0100

    Create Terraform config for EC2 instances
    Session 2 commit

--- Session-2-Create-EC2-Instance/main.tf
+++ Session-2-Create-EC2-Instance/main.tf
@@ -1,1 +1,28 @@
 
+# Simple Terraform configuration to create an AWS EC2 instance
+resource "aws_instance" "Prod" {
+  # ami         = "ami-0150189e4c09ffab5" # Example Amazon Linux 2023 AMI (verify for your region)
+  ami           = var.ami_id
+  # instance_type = "t3.micro"
+  instance_type = var.instance_type
+  
+  tags = {
+    Name        = "Prod-Instance"
+    Environment = "Production"
+  }
+ 
+}
+
+resource "aws_instance" "Dev" {
+  # ami         = "ami-0150189e4c09ffab5" # Example Amazon Linux 2023 AMI (verify for your region)
+  ami           = var.ami_id
+  # instance_type = "t3.micro"
+  instance_type = var.instance_type
+  
+  tags = {
+    Name        = "Dev-Instance"
+    Environment = "Development"
+  }
+ 
+}
+
